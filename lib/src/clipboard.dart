83a58b1b4ca75d5c08c02734702dbe174199548a
Kaan Alkan <105636211+ikuruzum@users.noreply.github.com>
2025-04-12 22:21:46 +0300

    made it wayland compatible

--- lib/src/clipboard.dart
+++ lib/src/clipboard.dart
@@ -11,7 +11,6 @@ abstract class Clipboard {
 }
 
 class MacClipboard implements Clipboard {
-  
   @override
   Future<bool> write(covariant String input) async {
     final process = await Process.start('pbcopy', [], runInShell: true);
@@ -42,11 +41,51 @@ class MacClipboard implements Clipboard {
   }
 }
 
+enum linuxDE {
+  x11,
+  wayland;
+
+  static linuxDE fromString(String name) {
+    return linuxDE.values.firstWhere((e) => e.name == name);
+  }
+}
+
+linuxDE? _manager;
+
 class LinuxClipboard implements Clipboard {
+  Future<bool> isCommandAvailable(String command) async {
+    final result = await Process.run('which', [command], runInShell: true);
+    return result.exitCode == 0;
+  }
+
+  Future<void> findManager() async {
+    if (_manager != null) return;
+    if (await isCommandAvailable('xsel')) {
+      _manager = linuxDE.x11;
+    } else if (await isCommandAvailable('wl-paste')) {
+      _manager = linuxDE.wayland;
+    } else {
+      throw Exception(
+          'No clipboard manager found install either xsel or wl-clipboard');
+    }
+  }
+
   @override
   Future<bool> write(covariant String input) async {
-    final process = await Process.start('xsel', ['--clipboard', '--input'],
-        runInShell: true);
+    await findManager();
+    Process process;
+    switch (_manager) {
+      case linuxDE.x11:
+        process = await Process.start('xsel', ['--clipboard', '--input'],
+            runInShell: true);
+        break;
+      case linuxDE.wayland:
+        process = await Process.start('wl-copy', [], runInShell: true);
+        break;
+      default:
+        throw Exception(
+            'No clipboard manager found install either xsel or wl-clipboard');
+    }
     process.stderr.transform(utf8.decoder).listen(print);
     process.stdin.write(input);
 
@@ -54,7 +93,7 @@ class LinuxClipboard implements Clipboard {
       await process.stdin.close();
     } catch (e) {
       print(
-          'daniboard needs [xsel] in Linux, please install it. Nothing was written to clipboard');
+          'daniboard needs [xsel] or [wl-clipboard] in Linux, please install it. Nothing was written to clipboard');
       return false;
     }
 
@@ -63,8 +102,21 @@ class LinuxClipboard implements Clipboard {
 
   @override
   Future<String> read() async {
-    final process = await Process.start('xsel', ['--clipboard', '--output'],
-        runInShell: true);
+    await findManager();
+    Process process;
+
+    switch (_manager) {
+      case linuxDE.x11:
+        process = await Process.start('xsel', ['--clipboard', '--output'],
+            runInShell: true);
+        break;
+      case linuxDE.wayland:
+        process = await Process.start('wl-paste', [], runInShell: true);
+        break;
+      default:
+        throw Exception(
+            'No clipboard manager found install either xsel or wl-clipboard');
+    }
     process.stderr.transform(utf8.decoder).listen(print);
 
     final stdout = process.stdout.transform(utf8.decoder);
@@ -77,8 +129,10 @@ class LinuxClipboard implements Clipboard {
   }
 }
 
-final winCopyPath = path.join(path.current, 'lib/src/backends/windows/copy.exe');
-final winPastePath = path.join(path.current, 'lib/src/backends/windows/paste.exe');
+final winCopyPath =
+    path.join(path.current, 'lib/src/backends/windows/copy.exe');
+final winPastePath =
+    path.join(path.current, 'lib/src/backends/windows/paste.exe');
 
 class WindowsClipboard implements Clipboard {
   @override
